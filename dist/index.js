378196df49faec39d76404e2982ffb952ccbebe6
Thomas Kim <thomaskim08310@gmail.com>
2024-08-01 09:14:07 -0400

    depth

--- dist/index.js
+++ dist/index.js
@@ -32,3 +32,27 @@ const user = {
     name: "Joe"
 };
 console.log(Direction1.Up);
+
+//just playing around
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      // Write your code here.
+      array.push(this.name)
+      for(const child of this.children){
+        child.depthFirstSearch(array)
+      }
+      return array
+    }
+  }
+
+  exports.Node = Node;
